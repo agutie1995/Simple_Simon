7bac6ff298b0a70fd61d9ec542bae72fdf430845
Alexandra Gutierrez <agutie95@yahoo.com>
2015-07-06 15:54:46 -0500

    edited content

---
+++ public/js/simple_simon.js
@@ -0,0 +1,33 @@
+'use strict';
+console.log('Welcome to Simple Simon');
+
+//activate start button
+
+//color 'blink'
+$('.color').click(function(event){
+    var squareClicked = $(this);
+    console.log(squareClicked);
+});
+
+//animate square when Simon or user clicks
+
+function animateSquare(color){
+    color.addClass('active');
+    setTimeout(function(){
+        color.removeClass('active');
+    }, 1000);
+};
+
+//generate random color between 0 and 3 from Simon
+
+function getRandomSquare(){
+    var squares = $('.color');
+    var random = Math.floor(Math.random() * 4);
+    console.log(random);
+}
+
+//remember and add to sequence
+
+//iterate animate each square in Simon's sequence
+
+//if user input is equal to Simon's sequence, continue playing; NOT equal to Simon's sequence, game over
