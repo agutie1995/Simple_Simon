719c2433ace32fad7598c5c5290185ef3f90f2be
Alexandra Gutierrez <agutie95@yahoo.com>
2015-07-07 10:41:37 -0500

    edited content

--- public/js/simple_simon.js
+++ public/js/simple_simon.js
@@ -1,36 +1,78 @@
 'use strict';
 console.log('Welcome to Simple Simon');
 
-//color 'blink'
-$('.color').click(function(event){
-    var squareClicked = $(this);
-    console.log(squareClicked);
+var simonSequence = [];
+var userInput = [];
+
+//activate start button
+
+$('#start').click(function(){
+    console.log('Let\'s play!')
+    getRandomColor();
+    playSimonSequence();
 });
 
-//animate square when Simon or user clicks
+//color 'blink'
 
-function animateSquare(color){
-    color.addClass('active');
+$('.color').click(function(event){
+    var idOfSquareClicked = $(this).attr('id');
+    animateColor(idOfSquareClicked);
+    console.log(idOfSquareClicked);
+});
+
+function animateColor(id){
+    $('#' + id).addClass('active');
     setTimeout(function(){
-        color.removeClass('active');
+        $('#' + id).removeClass('active');
     }, 1000);
 };
 
 //generate random color between 0 and 3 from Simon
 
-function getRandomSquare(){
-    var squares = $('.color');
-    var random = Math.floor(Math.random() * 4);
-    console.log(random);
-}
+function getRandomColor(){
+    var colors = $('.color');
+    var randomColor =  Math.floor(Math.random() * 4);
+    var buttonToAnimate = colors[randomColor];
+    var id = buttonToAnimate.getAttribute('id');
+    return animateColor(id);
+    simonSequence.push(id);
+};
+
+//animate square when Simon or user clicks
+
+$('.color').click(function(event){
+    var colorClicked = $(this);
+    console.log(colorClicked);
+});
 
 //remember and add to sequence
+//iterate each square in Simon's sequence
 
-//iterate animate each square in Simon's sequence
+function playSimonSequence(){
+    var i = 0;
+    var intervalId = setInterval(function(){
+    if (i >= simonSequence.length){
+            clearInterval(intervalId);
+        }
+        animateColor(simonSequence[i]);
+        i += 1;
+        }, 1000);
+};
 
-//if user input is equal to Simon's sequence, continue playing; NOT equal to Simon's sequence, game over
+//addEventListener to register user's click
 
-//activate start button
-$('#start').click(function(){
-    startSimonGame();
-})
+//$('.color').click(function(event){
+//    var colorClicked = $(this);
+//    animateColor(colorClicked);
+//});
+
+//if user input is equal to Simon's sequence, continue to next round; NOT equal to Simon's sequence, game over/alert user
+
+function gameOver(){
+    if (userInput.length == simonSequence.length){
+        playSimonSequence
+    }
+    if (userinput.length !== simonSequence.length){
+        alert('GAME OVER.')
+    };
+}
