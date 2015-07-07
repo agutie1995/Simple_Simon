ad0faf0619147f37494ffb4f88b46b208cf8df4a
Alexandra Gutierrez <agutie95@yahoo.com>
2015-07-07 15:13:21 -0500

    edited content

--- public/js/simple_simon.js
+++ public/js/simple_simon.js
@@ -2,30 +2,36 @@
 console.log('Welcome to Simple Simon');
 
 var simonSequence = [];
-var userInput = [];
+var userIndex = 0;
 
-//activate start button
-
-$('#start').click(function(){
-    console.log('Let\'s play!')
-    getRandomColor();
-    playSimonSequence();
-});
-
-//color 'blink'
 
 $('.color').click(function(event){
-    var idOfSquareClicked = $(this).attr('id');
-    animateColor(idOfSquareClicked);
-    console.log(idOfSquareClicked);
+    var idOfColorClicked = $(this).attr('id');
+    animateColor(idOfColorClicked);
+    //console.log(idOfColorClicked);
+//if user input is equal to Simon's sequence, continue to next round; NOT equal to Simon's sequence, game over
+    if (idOfColorClicked == simonSequence[userIndex]){
+        userIndex++;
+        if (userIndex == simonSequence.length){
+        newRound();
+        userIndex = 0;
+        } else {
+            alert('YOU LOSE!')
+        }
+    };
 });
+
+
+//color blink when clicked
+    //color blink when user clicks
 
 function animateColor(id){
     $('#' + id).addClass('active');
     setTimeout(function(){
         $('#' + id).removeClass('active');
-    }, 1000);
+    }, 750);
 };
+
 
 //generate random color between 0 and 3 from Simon
 
@@ -34,16 +40,16 @@ function getRandomColor(){
     var randomColor =  Math.floor(Math.random() * 4);
     var buttonToAnimate = colors[randomColor];
     var id = buttonToAnimate.getAttribute('id');
-    return animateColor(id);
+    //return animateColor(id);
     simonSequence.push(id);
+    console.log(simonSequence);
 };
 
-//animate square when Simon or user clicks
-
-$('.color').click(function(event){
-    var colorClicked = $(this);
-    console.log(colorClicked);
-});
+//new round/Simon's turn
+function newRound(){
+    playSimonSequence();
+    getRandomColor();
+};
 
 //remember and add to sequence
 //iterate each square in Simon's sequence
@@ -59,20 +65,15 @@ function playSimonSequence(){
         }, 1000);
 };
 
-//addEventListener to register user's click
+$('.color').click(function(event){
+    var colorClicked = $(this);
+    console.log(colorClicked);
+});
 
-//$('.color').click(function(event){
-//    var colorClicked = $(this);
-//    animateColor(colorClicked);
-//});
+//activate start button
 
-//if user input is equal to Simon's sequence, continue to next round; NOT equal to Simon's sequence, game over/alert user
-
-function gameOver(){
-    if (userInput.length == simonSequence.length){
-        playSimonSequence
-    }
-    if (userinput.length !== simonSequence.length){
-        alert('GAME OVER.')
-    };
-}
+$('#start').click(function(){
+    console.log('Let\'s play!');
+    simonSequence =[];
+    newRound();
+});
